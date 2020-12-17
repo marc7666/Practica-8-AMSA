657ad04a5de3d68bf6082e52408e717796ed73f2
marc7666 <64266158+marc7666@users.noreply.github.com>
2020-12-17 15:16:11 +0100

    Update prioritza.c

--- prioritza.c
+++ prioritza.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
 	printf("Assign to his father (shell) an execution priority of 30 \n");
 	int shell;
 	scheduling_parameters.sched_priority = 30; //Priority assignation
-	shell = sched_setscheduler(getpid(), SCHED_RR, &scheduling_parameters); //Policy assignation
+	shell = sched_setscheduler(getppid(), SCHED_RR, &scheduling_parameters); //Policy assignation
 	green();
 	printf("OK 2\n");
 
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]){
 	printf("Assign to the parameter process an execution priority of 10 \n");
 	int process_param;
 	scheduling_parameters.sched_priority = 10; //Priority assignation
-	process_param = sched_setscheduler(getpid(), SCHED_RR, &scheduling_parameters); //Policy assignation
+	process_param = sched_setscheduler(pid, SCHED_RR, &scheduling_parameters); //Policy assignation
 	green();
 	printf("OK 3\n");
 
